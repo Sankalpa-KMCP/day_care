8e606b94332f8ab53c26d390ae89b5dd35dbd184
Sankalpa KMCP <it25100142@my.sliit.lk>
2026-08-26 11:08:05 +0530

    chore: update config and branding

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS
-    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'day_care'}/`
-    : '/',
 })
